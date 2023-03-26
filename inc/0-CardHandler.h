4d448d3b1f137718edba0eef70e10918cd5909b0
15git-ruimendes <up201707154@fe.up.pt>
2023-03-26 23:50:39 +0100

    Card handler structures

---
+++ inc/0-CardHandler.h
@@ -0,0 +1,106 @@
+/**
+ * @file 0-CardHandler.h
+ * @author Rui Mendes
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-26
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef _CARD_HANDLER_
+#define _CARD_HANDLER_
+
+/*
+ * Private Includes
+ */
+
+//#include "main.h"
+
+/*
+ * Private Definitions 
+ */
+
+#define MASTER_CODE
+
+#define SAC_A_CODE
+#define SAC_B_CODE
+#define IIS_A_CODE
+#define IIS_B_CODE
+#define TAS_CODE
+#define DIGC_CODE
+#define COM_CODE
+
+#define START
+#define STOP
+#define RECALL
+
+/*
+ * Private Structures
+ */
+
+/**
+ * @brief  Generic Card value structure
+ * @note   
+ * @retval None
+ */
+typedef struct 
+{
+    uint32_t CAN_Code;
+    
+    uint32_t Last_RxData;
+
+    uint32_t Last_TxData;
+
+    uint32_t State;
+
+    uint32_t Last_Events;    
+}GenCard;
+
+/**
+ * @brief Master card, system backend handler  
+ * @note   
+ * @retval None
+ */
+typedef struct 
+{
+    GenCard SACA;
+    GenCard SACB;
+
+    GenCard IISA;
+    GenCard IISB;
+
+    GenCard TAS;
+    GenCard DIGC;
+
+}CardHandler;
+
+/*
+ * Private Functions
+ */
+void idBuilder();
+void idParser();
+
+/*
+ * Public Functions
+ */
+void StartCard();
+void StopCard();
+void RecallCard();
+
+void StartCard_T();
+void StopCard_T();
+void RecallCard_T();
+
+void ParseTemperature();
+void ParseSensors();
+
+void SwitchKPC();
+void SwitchMain();
+void SwitchDC();
+
+void SwitchOutputs();
+void ParseInputs();
+
+#endif
---
+++ src/0-CardHandler.c
@@ -0,0 +1,10 @@
+/**
+ * @file 0-CardHandler.c
+ * @author Rui Mendes 
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-26
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
